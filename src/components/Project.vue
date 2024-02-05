76c89ffdea09e9bfc5dec05ba3e4f129187c933b
kingjamesx <talk2james.uj@gmail.com>
2024-02-05 11:28:15 +0100

    add open-new tab to links

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,69 +1,70 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
-import BaseButton from './BaseButton.vue';
+import { ref } from "vue";
+import BaseButton from "./BaseButton.vue";
 
+const props = defineProps({
+  title: {
+    title: "String",
+    default: "Name of Website",
+  },
+  subtitle: {
+    subtitle: "string",
+    default: "i built this",
+  },
+  img: {
+    img: "String",
+    default: "@/assets/images/test-img.png",
+  },
+  url: {
+    url: "String",
+    default: "https://moviesearchflix.netlify.app/",
+  },
+});
 
-const props=defineProps({
-    title:{
-        title:'String',
-        default:'Name of Website',
-    },
-   subtitle:{
-    subtitle:"string",
-    default:'i built this'
-   },
-    img:{
-        img:'String',
-        default:'@/assets/images/test-img.png',
-    },
-    url:{
-        url:'String',
-        default:'https://moviesearchflix.netlify.app/',
-    }
-})
- 
-const order=ref(props)
-console.log(order.value)
+const order = ref(props);
+console.log(order.value);
 </script>
 <template>
-    <div class="box">
-        <span class="image"><img :src="props.img"  alt="project hero"></span>
-        <div class="content ">
-            <h2>{{ title }}</h2>
-            <p class="sub-title">{{ subtitle }}</p>
-  <a :href="props.url"><base-button>View Project</base-button></a>
-        </div>
+  <div class="box">
+    <span class="image"><img :src="props.img" alt="project hero" /></span>
+    <div class="content">
+      <h2>{{ title }}</h2>
+      <p class="sub-title">{{ subtitle }}</p>
+      <a :href="props.url" target="_blank"
+        ><base-button>View Project</base-button></a
+      >
     </div>
+  </div>
 </template>
 <style lang="scss">
-   .box{
-    margin-top: 48px;
+.box {
+  margin-top: 48px;
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+}
+.content {
+  flex-basis: 40%;
+}
+.image {
+  flex-basis: 40%;
+  display: block;
+}
+.reverse {
+  flex-direction: row-reverse;
+}
+@media (max-width: 957px) {
+  .box {
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem;
+  }
+
+  .content {
     display: flex;
-    justify-content: center;
-    gap: 50px;
-   }
-   .content{
-    flex-basis: 40%;
-   }
-   .image{
-    flex-basis: 40%;
-    display: block;
-   }
-   .reverse{
-    flex-direction: row-reverse;
-   }
-   @media (max-width:957px) {
-    .box{
-        flex-direction: column;
-        align-items: center;
-        padding: 2rem;
-    }
- 
-    .content{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        text-align: center;
-    }
-   }
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+}
 </style>
